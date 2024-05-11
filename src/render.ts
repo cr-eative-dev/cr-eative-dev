163f1706e4471fddc5675bf836177a5d062e4027
Florian T. <floriantappeser@gmail.com>
2024-05-11 12:32:17 +0200

    chhanges

--- src/render.ts
+++ src/render.ts
@@ -2,7 +2,7 @@ import type { Year } from './worker';
 
 const BP_MEDIUM = 550;
 const BP_LARGE = 700;
-const BODY_COPY = `I'm Terkel. A Brooklyn-based internet enthusiast, creative technologist and maker of digital ephemera.`;
+const BODY_COPY = `I'm Florian. A full-stack creative developer and 3D artist.`;
 
 interface Props {
   width?: number;
