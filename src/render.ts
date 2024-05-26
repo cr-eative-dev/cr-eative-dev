e1e9197b39cf8d1b4bcca824cd59f590a7d25074
𝚌𝚛.𝚎𝚊𝚝𝚒𝚟𝚎.𝚍𝚎𝚟 / <floriantappeser@gmail.com>
2024-05-26 18:48:00 +0200

    Update render.ts

--- src/render.ts
+++ src/render.ts
@@ -2,7 +2,7 @@ import type { Year } from './worker';
 
 const BP_MEDIUM = 550;
 const BP_LARGE = 700;
-const BODY_COPY = `Software Developer, 3D Technical Artist and Creative Director. I love blending design with technology to craft innovative solutions.`;
+const BODY_COPY = `Software Developer & 3D Artist blending design with technology to craft innovative solutions.`;
 
 interface Props {
   width?: number;
